ac3d3b2d593cdc92168a0464330138082a188f29
Wagner do Amparo Santana <38575562+wagnersantan@users.noreply.github.com>
2025-03-27 23:01:15 -0300

    Update dashbod_torneio.py

--- sr/dashbod_torneio.py
+++ sr/dashbod_torneio.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import io
 
 # --- Título do aplicativo ---
-st.title("🏆 Torneio de Xadrez Ilhéus/Itabuna Online - 31/03/2025")
+st.title("🏆 Torneio de Xadrez Ilhéus/Itabuna Online - 23/03/2025")
 st.write("Bem-vindo ao painel de análise do torneio de xadrez!")
 
 # --- Upload do arquivo pelo usuário ---
